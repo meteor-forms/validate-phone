832fd256231721d9b56c180e04138dd12876b761
Patrick Lewis <Patrickml.pml@gmail.com>
2015-10-12 15:17:51 -0400

    Fixed Package.js

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'forms:validate-phone',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: 'https://github.com/meteor-forms/validate-phone',
+  summary: 'Validate Phone Number Fields in Meteor Forms',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/meteor-forms/validate-phone',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
